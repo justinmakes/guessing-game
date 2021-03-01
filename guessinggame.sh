49e7cca2516c29089fe581451f5ec5648135443f
justinr <justin@apsis.co>
2021-03-01 15:40:33 -0700

    added function to guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,33 +1,40 @@
 #!/bin/bash
 
-#determine fileCount in directory
-fileCount=$(ls | wc -l)
+#declare function
+function guessinggame {
 
-#initialize guess to 0
-guess=0
-
-if [[ $fileCount -eq 0 ]]
-then
-	echo "This directory is empty!"
-else
-	#while guess does not equal fileCount
-	while [[ $guess -ne $fileCount ]]
-	do
-		#prompt user to enter guess as int
-		read -p "How many files are present in this directory? " guess
-		
-		#if guess is high inform user
-		if [[ $guess -gt $fileCount ]]
-		then
-			echo "Your entry was too high!"
-
-		#if guess is low inform user
-		elif [[ $guess -lt $fileCount ]]
-		then
-			echo "Your entry was too low!"
-		fi
+	#determine fileCount in directory
+	fileCount=$(ls | wc -l)
 	
-	done
-	#Congratulate user on correct guess!
-	echo "Your entry was correct!"
-fi
+	#initialize guess to 0
+	guess=0
+	
+	if [[ $fileCount -eq 0 ]]
+	then
+		echo "This directory is empty!"
+	else
+		#while guess does not equal fileCount
+		while [[ $guess -ne $fileCount ]]
+		do
+			#prompt user to enter guess as int
+			read -p "How many files are present in this directory? " guess
+			
+			#if guess is high inform user
+			if [[ $guess -gt $fileCount ]]
+			then
+				echo "Your entry was too high!"
+	
+			#if guess is low inform user
+			elif [[ $guess -lt $fileCount ]]
+			then
+				echo "Your entry was too low!"
+			fi
+		
+		done
+		#Congratulate user on correct guess!
+		echo "Your entry was correct!"
+	fi
+}
+
+#call function
+guessinggame
